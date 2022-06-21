6227212d13bdfd7e7a14d0d30fe29f0fdc76d2f1
whrksp126 <whrksp126@naver.com>
2022-06-21 23:41:00 +0900

    useState로 이미지 파일 데이터 관리하기기

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import UploadForm from './components/UploadForm'
+
+const App = () => {
+  return (
+    <div>
+      <h2>사진첩</h2>
+      <UploadForm />
+    </div>
+  );
+}
+
+export default App;
