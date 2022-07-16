37856783fa2fe92d39cea51d85921727f261ea94
whrksp126 <whrksp126@naver.com>
2022-07-16 15:13:26 +0900

    toolbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,19 @@ import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
 import RegisterPage from './pages/RegisterPage';
 import { Route, Routes} from 'react-router-dom';
+import ToolBar from './components/ToolBar';
 
 const App = () => {
 
   return (
     <div style={{maxWidth:600, margin: "auto"}}>
       <ToastContainer />
-        <Routes>
-          <Route path="/auth/register" element={<RegisterPage />} exact />
-          <Route path="/auth/login" element={<LoginPage />} exact />
-          <Route path="/" element={<MainPage />} />
-        </Routes>
+      <ToolBar />
+      <Routes>
+        <Route path="/auth/register" element={<RegisterPage />} exact />
+        <Route path="/auth/login" element={<LoginPage />} exact />
+        <Route path="/" element={<MainPage />} />
+      </Routes>
     </div>
   );
 }
